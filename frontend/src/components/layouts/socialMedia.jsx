56ad1a71a43d824dd4c4fd7b47996808905fd2cb
YANNDARK <iansantosmcqueen@gmail.com>
2024-04-18 23:52:14 -0300

    commit 18/04

--- frontend/src/components/layouts/socialMedia.jsx
+++ frontend/src/components/layouts/socialMedia.jsx
@@ -1,4 +1,5 @@
 import styles from './social_media.module.scss';
+import { FaWhatsapp, FaInstagram, FaBehance } from 'react-icons/fa';
 
 import behance from '../../images/square-behance.svg'
 import wpp from '../../images/square-wpp.svg'
@@ -7,24 +8,26 @@ import insta from '../../images/square-insta.svg'
 
 
 
-const SocialMedia = ({customClass}) => {
+const SocialMedia = ({ customClass }) => {
 
     return (
         <ul className={styles.container_network}>
             <span></span>
             <li className={`${styles.item} ${styles.insta_item} ${styles[customClass]}`}>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=mV0ST0aGfAo&ab_channel=Bert-Topic" title="Contact">
-                    <img className={styles.img_insta} src={insta} alt="loho_insta" /></a>
+                <a href='#'>
+                    <FaInstagram size={20} />
+                </a>
             </li>
 
             <li className={`${styles.item} ${styles.wpp_item}`}>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=mV0ST0aGfAo&ab_channel=Bert-Topic" title="Contact">
-                    <img className={styles.img_wpp} src={wpp} alt="wpp" /></a>
+                <a href='#'>
+                    <FaWhatsapp size={20} />
+                </a>
             </li>
 
             <li className={`${styles.item} ${styles.behance}`}>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=mV0ST0aGfAo&ab_channel=Bert-Topic" title="Contact">
-                    <img className={styles.img_behance} src={behance} alt="Behance" />
+                <a href='#'>
+                    <FaBehance size={20} />
                 </a>
             </li>
 
