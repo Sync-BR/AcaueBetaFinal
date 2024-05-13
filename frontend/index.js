251ca1f8e5736eaf6b10d255bb7977c974db3a12
yanndarkz <iansantosmcqueen@gmail.com>
2024-05-12 22:38:58 -0300

    frontend/12/05

--- frontend/index.js
+++ frontend/index.js
@@ -10,6 +10,14 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename);
 
+// socket
+const socket = new WebSocket("ws://177.208.2.109:3000");
+
+// Listen for possible errors
+socket.addEventListener("error", (event) => {
+  console.log("WebSocket error: ", event);
+});
+
 app.use(express.static(join(__dirname, 'dist')));
 app.get('*', (req, res) => {
     res.sendFile(join(__dirname, 'dist', 'index.html'));
